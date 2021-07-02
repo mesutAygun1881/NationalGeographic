3ecc019f0668dfd5286f0e7b611172f37eea4aec
mesutAygun1881 <mesut>
2021-07-02 19:54:01 +0300

    Initial Commit

---
+++ National Geographic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  National Geographic
+//
+//  Created by Mesut Aygün on 2.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
